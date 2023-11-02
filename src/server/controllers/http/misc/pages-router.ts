7e246f184244648f3f251910eabcd9cf6f8c8f66
Julien Maulny <julien.maulny@protonmail.com>
2023-11-02 22:39:09 +0100

    umami

--- src/server/controllers/http/misc/pages-router.ts
+++ src/server/controllers/http/misc/pages-router.ts
@@ -7,8 +7,17 @@ export function pagesRouter() {
     // Render main page for any routes for html5 navigation
     router.get('/**', async (_, res) => {
         const manifest = await getManifest();
+
+        const umami = {
+            websiteId: process.env.UMAMI_WEBSITE_ID,
+            src: process.env.UMAMI_SRC,
+            domains: process.env.UMAMI_DOMAINS,
+            hostUrl: process.env.UMAMI_HOST_URL,
+        };
+
         res.render('page.ejs', {
             manifest,
+            umami,
         });
     });
 
