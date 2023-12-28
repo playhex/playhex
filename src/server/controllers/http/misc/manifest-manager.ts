2c694174399b3a8c097ec4399d448240143e4747
Julien Maulny <julien.maulny@protonmail.com>
2023-12-28 23:45:06 +0100

    Prevent including source maps as javascript

--- src/server/controllers/http/misc/manifest-manager.ts
+++ src/server/controllers/http/misc/manifest-manager.ts
@@ -2,30 +2,50 @@ import fs from 'fs';
 import path from 'path';
 import { IS_DEV, WEBPACK_PORT } from '../../../config';
 
-/* eslint-disable */
+type ManifestType = { [key: string]: string };
 
-async function getManifestFromWebpack(): Promise<string> {
+/**
+ * Do not include source maps in manifest to not load them as javascripts
+ */
+const removeSourceMaps = (manifest: ManifestType): void => {
+    Object.keys(manifest)
+        .forEach(key => {
+            if (key.endsWith('.map')) {
+                delete manifest[key];
+            }
+        })
+    ;
+};
+
+const getManifestFromWebpack = async (): Promise<ManifestType> => {
     const response = await fetch(`http://localhost:${WEBPACK_PORT}/statics/manifest.json`);
+    const manifest: ManifestType = await response.json();
 
-    return response.text();
-}
+    removeSourceMaps(manifest);
 
-let manifestStrCache: string;
+    return manifest;
+};
 
-export async function getManifest() {
-    let manifestStr: string;
+let manifestCache: null | ManifestType = null;
+
+export async function getManifest(): Promise<ManifestType> {
     if (IS_DEV) {
         // load from webpack dev server
-        manifestStr = await getManifestFromWebpack();
-    } else {
-        if (manifestStrCache) {
-            manifestStr = manifestStrCache;
-        } else {
-            // read from file system
-            manifestStr = fs.readFileSync(path.join(process.cwd(), 'dist', 'statics', 'manifest.json'), 'utf-8').toString();
-            manifestStrCache = manifestStr;
-        }
+        return getManifestFromWebpack();
     }
-    const manifest = JSON.parse(manifestStr);
-    return manifest;
+
+    if (null !== manifestCache) {
+        return manifestCache;
+    }
+
+    // read from file system
+    manifestCache = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'dist', 'statics', 'manifest.json'), 'utf-8').toString());
+
+    if (!manifestCache) {
+        throw new Error('"manifest.json" file not found');
+    }
+
+    removeSourceMaps(manifestCache);
+
+    return manifestCache;
 }
