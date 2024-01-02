75e4926eee8a743ddbc77082c22c4d347c2e8dd1
Julien Maulny <julien.maulny@protonmail.com>
2024-01-02 21:52:12 +0100

    Try to fix bug where colorA and colorB are undefined

--- src/client/pixi-board/BoardTheme.ts
+++ src/client/pixi-board/BoardTheme.ts
@@ -1,39 +1,31 @@
-import { themeSwitcherDispatcher } from '@client/DarkThemeSwitcher';
+import { getTheme, themeSwitcherDispatcher } from '@client/DarkThemeSwitcher';
 
 export type Theme = {
-    colorA: number;
-    colorB: number;
-    colorEmpty: number;
-    strokeColor: number;
-    textColor: number;
+    colorA: number; // --bs-danger
+    colorB: number; // --bs-primary
+    colorEmpty: number; // --bs-light-bg-subtle
+    strokeColor: number; // --bs-dark-bg-subtle
+    textColor: number; // --bs-body-color
 };
 
-const rootStyles = window.getComputedStyle(document.body);
-
-const colorFromCss = (color: string): number => {
-    let colorStr = rootStyles.getPropertyValue(color);
-    let tokens: null | RegExpMatchArray;
-
-    if (null !== (tokens = colorStr.match(/^#(.)(.)(.)$/))) {
-        const [, r, g, b] = tokens;
-        colorStr = '#' + r + r + g + g + b + b;
-    }
-
-    return parseInt(colorStr.substring(1), 16);
+const darkTheme: Theme = {
+    colorA: 0xdc3545,
+    colorB: 0x0d6efd,
+    colorEmpty: 0x343a40,
+    strokeColor: 0x1a1d20,
+    textColor: 0xdee2e6,
 };
 
-export const currentTheme: Theme = {
-    colorA: colorFromCss('--bs-danger'),
-    colorB: colorFromCss('--bs-primary'),
-    colorEmpty: colorFromCss('--bs-light-bg-subtle'),
-    strokeColor: colorFromCss('--bs-dark-bg-subtle'),
-    textColor: colorFromCss('--bs-body-color'),
+const lightTheme: Theme = {
+    colorA: 0xdc3545,
+    colorB: 0x0d6efd,
+    colorEmpty: 0xfcfcfd,
+    strokeColor: 0xced4da,
+    textColor: 0x212529,
 };
+
+export let currentTheme: Theme = getTheme() === 'dark' ? darkTheme : lightTheme;
 
 themeSwitcherDispatcher.on('themeSwitched', () => {
-    currentTheme.colorA = colorFromCss('--bs-danger');
-    currentTheme.colorB = colorFromCss('--bs-primary');
-    currentTheme.colorEmpty = colorFromCss('--bs-light-bg-subtle');
-    currentTheme.strokeColor = colorFromCss('--bs-dark-bg-subtle');
-    currentTheme.textColor = colorFromCss('--bs-body-color');
+    currentTheme = getTheme() === 'dark' ? darkTheme : lightTheme;
 });
