8088cfc28e1e3cfb2eee938ee59dffaba87162f6
Julien Maulny <julien.maulny@protonmail.com>
2024-02-15 21:07:04 +0100

    Compatibility with node 18 to 21

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 /* eslint-disable no-undef */
 
-const versionRequired = 18;
-const [v, major] = process.version.match(/(\d+)\.\d+\.\d+/);
+const versionRequired = [18, 18, 2]; // on node 18.18, "import=ts-node/esm" has replaced "loader=ts-node/esm"
+const [currentVersion, major, minor, patch] = process.version.match(/(\d+)\.(\d+)\.(\d+)/);
 
-if (parseInt(major, 10) < versionRequired) {
-    throw `Node >= ${versionRequired} required, actual version: ${v}.`;
+if (parseInt(major, 10) <= versionRequired[0]
+    && parseInt(minor, 10) <= versionRequired[1]
+    && parseInt(patch, 10) < versionRequired[2]
+) {
+    throw new Error(`Node >= ${versionRequired.join('.')} required, actual version: ${currentVersion}.`);
 }
 
 require('./dist/server');
