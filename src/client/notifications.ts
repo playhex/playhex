1f08c1987a6711a7c4b3427e4dfb54f41f3c239c
Julien Maulny <julien.maulny@protonmail.com>
2024-03-23 20:56:46 +0100

    add typings, catch notification error on mobile

--- src/client/notifications.ts
+++ src/client/notifications.ts
@@ -1,14 +1,18 @@
 const icon = '/images/logo-transparent.svg';
 
-const sendNotification = (options, route) => {
+const sendNotification = (options: NotificationOptions, route: () => void) => {
     if (Notification.permission === 'default') {
         Notification.requestPermission();
     } else if (Notification.permission === 'granted') {
-        new Notification('PlayHex', { ...options, icon }).onclick = function() {
-            if (route) route();
-            focus(window);
-            this.close();
-        };
+        try {
+            new Notification('PlayHex', { ...options, icon }).onclick = function() {
+                if (route) route();
+                focus();
+                this.close();
+            };
+        } catch (e) {
+            // TODO check compatibility on mobile. https://stackoverflow.com/questions/29774836/failed-to-construct-notification-illegal-constructor
+        }
     }
 };
 
