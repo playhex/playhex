6d5b33666b7c08fdca90f7cd1dea4a32e5f31d9d
Julien Maulny <julien.maulny@protonmail.com>
2024-04-23 21:46:35 +0200

    pwa

--- src/server/controllers/http/misc/pwa-router.ts
+++ src/server/controllers/http/misc/pwa-router.ts
@@ -6,6 +6,7 @@ export function pwaRouter() {
 
     router.get('/pwa-manifest.json', async (_, res) => {
         const pwaManifest = {
+            id: '/',
             short_name: seo.title,
             name: seo.title,
             description: seo.description,
@@ -14,9 +15,15 @@ export function pwaRouter() {
             start_url: '/',
             theme_color: '#DC3545',
             background_color: '#212529',
-            display: 'standalone',
+            display: 'fullscreen',
+            display_override: ['window-controls-overlay', 'fullscreen', 'minimal-ui', 'standalone'],
             scope: '/',
             prefer_related_applications: false,
+            categories: ['games', 'entertainment'],
+            dir: 'auto',
+            launch_handler: {
+                client_mode: ['navigate-existing', 'auto'],
+            },
             shortcuts: [
                 {
                     name: 'Play offline vs AI',
@@ -36,20 +43,57 @@ export function pwaRouter() {
                 {
                     src: '/images/screenshots/game.png',
                     type: 'image/png',
-                    sizes: '1368x918',
+                    sizes: '3156x1863',
                     form_factor: 'wide',
+                    label: 'Game view with analyze by AI',
+                },
+                {
+                    src: '/images/screenshots/game-narrow.png',
+                    type: 'image/png',
+                    sizes: '1080x2172',
+                    form_factor: 'narrow',
+                    label: 'Play Hex',
+                },
+                {
+                    src: '/images/screenshots/create-game-narrow.png',
+                    type: 'image/png',
+                    sizes: '1491x2436',
+                    form_factor: 'narrow',
+                    label: 'Create game vs AI',
+                },
+                {
+                    src: '/images/screenshots/review-game.png',
+                    type: 'image/png',
+                    sizes: '3936x2436',
+                    form_factor: 'wide',
+                    label: 'Analyze game with AI',
+                },
+                {
+                    src: '/images/screenshots/review-game-narrow.png',
+                    type: 'image/png',
+                    sizes: '1491x2436',
+                    form_factor: 'narrow',
+                    label: 'Analyze game with AI',
                 },
             ],
             icons: [
                 {
                     src: '/images/logo-transparent.svg',
                     type: 'image/svg+xml',
-                    sizes: 'any',
+                    sizes: '512x512',
+                    purpose: 'any',
                 },
                 {
-                    src: '/images/splash.png',
-                    type: 'image/png',
+                    src: '/images/logo-transparent.svg',
+                    type: 'image/svg+xml',
+                    sizes: 'any',
+                    purpose: 'any',
+                },
+                {
+                    src: '/images/logo-maskable.svg',
+                    type: 'image/svg+xml',
                     sizes: '512x512',
+                    purpose: 'maskable',
                 },
                 {
                     src: '/images/logo-maskable.svg',
