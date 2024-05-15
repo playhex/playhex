dc898a37ca93f437949a8333752148f60d67acdd
Julien Maulny <julien.maulny@protonmail.com>
2024-05-15 22:03:08 +0200

    command to anonymize database

---
+++ src/server/commands/anonymizeDB.ts
@@ -0,0 +1,33 @@
+import readline from 'node:readline/promises';
+import { stdin, stdout } from 'node:process';
+import { hashPassword } from '../services/security/authentication';
+import { AppDataSource } from '../data-source';
+import hexProgram from './hexProgram';
+
+hexProgram
+    .command('db-anonymize')
+    .description('Anonymize database personal data for development purpose. Must not be run on production for sure.')
+    .action(async () => {
+        if (!AppDataSource.isInitialized) {
+            await AppDataSource.initialize();
+        }
+
+        const rl = readline.createInterface({ input: stdin, output: stdout });
+        const answer = await rl.question('This will remove data, not to do on real database! Are you sure? Type "yes": ');
+
+        rl.close();
+
+        if ('yes' !== answer) {
+            console.log('Nothing has been done (type "yes" if you wanted to).');
+            return;
+        }
+
+        console.log('Replacing all account passwords to "test"...');
+        AppDataSource.query(
+            'update Player set password = ? where password is not null',
+            [await hashPassword('test')],
+        );
+
+        console.log('done');
+    })
+;
--- src/server/commands/index.ts
+++ src/server/commands/index.ts
@@ -3,5 +3,6 @@ import hexProgram from './hexProgram';
 import './createRandomBotsCommand';
 import './createMohexBotsCommand';
 import './createKatahexBotsCommand';
+import './anonymizeDB';
 
 hexProgram.parse();
