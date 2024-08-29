40bd87e078e519bf3b9357ee4cc6b3b3194e44e5
Julien Maulny <julien.maulny@protonmail.com>
2024-08-29 20:53:30 +0200

    Move cypress test in relevant file

--- cypress/e2e/chat.cy.ts
+++ cypress/e2e/chat.cy.ts
@@ -37,4 +37,21 @@ describe('Chat', () => {
 
         cy.contains('.chat-messages .text-body', 'Guest 7614').should('not.visible');
     });
+
+    it('shows chat message length limit when I am about to reach it, instead of crashing when sending a too long message', () => {
+        cy.intercept('/api/games/00000000-0000-0000-0000-000000000000', {
+            fixture: 'game/game-playing.json',
+        });
+        cy.intercept('/api/auth/me-or-guest', {
+            fixture: 'game/me-or-guest.json',
+        });
+
+        cy.visit('/games/00000000-0000-0000-0000-000000000000');
+
+        cy.contains('Loading game 00000000-0000-0000-0000-000000000000…').should('not.exist');
+
+        cy.contains(/\d+ \/ 250 characters/).should('not.exist');
+        cy.get('.chat-input input').click().type('I am writing a message too long, Quod natus vitae soluta officia. Laboriosam assumenda velit quasi et laboriosam magnam occaecati. Et similique aspernatur rerum est ipsa. Aut at ut itaque. Odio sed non qui aut minima sed vel. Deleniti illo dolorum', { timeout: 10000 });
+        cy.contains('247 / 250 characters');
+    });
 });
--- cypress/e2e/page-game.cy.ts
+++ cypress/e2e/page-game.cy.ts
@@ -12,23 +12,6 @@ describe('Page Game', () => {
         // Cannot click with cypress, seems to be handled in a way it cannot make assertions.
     });
 
-    it('shows chat message length limit when I am about to reach it, instead of crashing when sending a too long message', () => {
-        cy.intercept('/api/games/00000000-0000-0000-0000-000000000000', {
-            fixture: 'game/game-playing.json',
-        });
-        cy.intercept('/api/auth/me-or-guest', {
-            fixture: 'game/me-or-guest.json',
-        });
-
-        cy.visit('/games/00000000-0000-0000-0000-000000000000');
-
-        cy.contains('Loading game 00000000-0000-0000-0000-000000000000…').should('not.exist');
-
-        cy.contains(/\d+ \/ 250 characters/).should('not.exist');
-        cy.get('.chat-input input').click().type('I am writing a message too long, Quod natus vitae soluta officia. Laboriosam assumenda velit quasi et laboriosam magnam occaecati. Et similique aspernatur rerum est ipsa. Aut at ut itaque. Odio sed non qui aut minima sed vel. Deleniti illo dolorum', { timeout: 10000 });
-        cy.contains('247 / 250 characters');
-    });
-
     it('displays remaining time for both players', () => {
         cy.intercept('/api/games/00000000-0000-0000-0000-000000000000', {
             fixture: 'game/game-playing.json',
