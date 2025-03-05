87aa12728d6c659e8ca6654dc39057908b1e2f53
Julien Maulny <julien.maulny@protonmail.com>
2025-03-05 20:29:15 +0100

    Fix move played sound not triggered when playing too fast

--- src/shared/app/audioPlayer.ts
+++ src/shared/app/audioPlayer.ts
@@ -1,16 +1,12 @@
-const audioSingletons: { [filename: string]: HTMLAudioElement } = {};
-
-const getAudioSingleton = (filename: string): HTMLAudioElement => {
-    if (!audioSingletons[filename]) {
-        audioSingletons[filename] = new Audio(filename);
-    }
-
-    return audioSingletons[filename];
+const getAudio = (filename: string): HTMLAudioElement => {
+    // must recreate new audio every time to prevent audio not playing because not finished last play
+    // e.g Move played too fast
+    return new Audio(filename);
 };
 
 export const playAudio = async (filename: string): Promise<void> => {
     try {
-        const audio = getAudioSingleton(filename);
+        const audio = getAudio(filename);
         await audio.play();
     } catch (e) {
         // noop, browser says user has not allowed audio permission
