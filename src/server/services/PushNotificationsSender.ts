7df661d4788f9e227a228ab628dcc9e8d8b7d3cf
Julien Maulny <julien.maulny@protonmail.com>
2025-03-09 16:57:27 +0100

    Restore uncommited push logging

--- src/server/services/PushNotificationsSender.ts
+++ src/server/services/PushNotificationsSender.ts
@@ -1,8 +1,9 @@
-import webPush from 'web-push';
+import webPush, { WebPushError } from 'web-push';
 import { Service } from 'typedi';
 import { Player } from '../../shared/app/models';
 import PlayerPushSubscriptionRepository from '../repositories/PlayerPushSubscriptionRepository';
 import { PushPayload } from '../../shared/app/PushPayload';
+import logger from './logger';
 
 const {
     PUSH_VAPID_PUBLIC_KEY,
@@ -41,13 +42,62 @@ export class PushNotificationSender
 
         const subscriptions = await this.playerPushSubscriptionRepository.findForPlayer(player);
 
-        return await Promise.allSettled(subscriptions.map(async subscription => {
+        if (0 === subscriptions.length) {
+            return;
+        }
+
+        let sent = 0;
+        let removed = 0;
+        let errors = 0;
+
+        const result = await Promise.allSettled(subscriptions.map(async subscription => {
             try {
-                return await webPush.sendNotification(subscription, JSON.stringify(pushPayload));
+                const response = await webPush.sendNotification(subscription, JSON.stringify(pushPayload));
+
+                ++sent;
+
+                return response;
             } catch (e) {
-                // TODO remove subscription when expired, or too many failed in a row
-                throw { ...e };
+                if (!(e instanceof WebPushError)) {
+                    logger.warning('sendNotification unexpected error type', {
+                        subscription,
+                        message: e.message,
+                    });
+
+                    ++errors;
+                } else {
+                    if (410 === e.statusCode) {
+                        await this.playerPushSubscriptionRepository.remove(subscription);
+
+                        ++removed;
+                    } else {
+                        logger.warning('sendNotification unexpected WebPushError', {
+                            subscription,
+                            statusCode: e.statusCode,
+                            errorMessage: e.body,
+                        });
+
+                        ++errors;
+                    }
+                }
+
+                throw e;
             }
         }));
+
+        logger.info('Sent push notification to player', {
+            playerPublicId: player.publicId,
+            notification: {
+                title: pushPayload.title,
+                body: pushPayload.body,
+            },
+            results: {
+                sent,
+                removed,
+                errors,
+            },
+        });
+
+        return result;
     }
 }
