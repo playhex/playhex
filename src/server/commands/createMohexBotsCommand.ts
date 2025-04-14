2e5de81fcbd4a7c12f7a243d411c05bd485b5b69
Julien Maulny <julien.maulny@protonmail.com>
2025-04-14 22:15:53 +0200

    Update createMohexBots command with new levels of Mohex added by hand

--- src/server/commands/createMohexBotsCommand.ts
+++ src/server/commands/createMohexBotsCommand.ts
@@ -7,54 +7,38 @@ hexProgram
     .action(async () => {
         let created: boolean;
 
-        const configs = [
-            { level: 1, maxGames: 8 },
-            { level: 2, maxGames: 20 },
-            { level: 3, maxGames: 100 },
-            { level: 4, maxGames: 400 },
-            { level: 5, maxGames: 1000 },
+        const configs: { level: string, maxGames: number, description?: string, pseudo?: string, slug?: string, label?: string }[] = [
+            { level: '1', maxGames: 8 },
+            { level: '1.5', maxGames: 12, slug: 'mohex-1-5' },
+            { level: '2', maxGames: 20 },
+            { level: '3', maxGames: 100 },
+            { level: '4', maxGames: 400 },
+            { level: '5', maxGames: 1000 },
+            { level: '6', maxGames: 2000, description: 'max 2000 sim., ~1s per move' },
+            { level: 'Full', label: 'Full', maxGames: 500000, description: 'not limited, ~25s per move', slug: 'mohex', pseudo: 'Mohex' },
         ];
 
-        configs.forEach(async ({ level, maxGames }) => {
-            const label = `Level ${level}`;
+        configs.forEach(async ({ level, maxGames, label, description, pseudo, slug }, index) => {
+            label ??= `Level ${level}`;
             console.log(`Creating bot "${label}"...`);
 
             created = await createAiConfigIfNotExists({
                 config: { maxGames },
                 engine: 'mohex',
                 label,
-                description: `max ${maxGames} simulations`,
-                order: 20 + level - 1,
+                description: description ?? `max ${maxGames} simulations`,
+                order: 20 + index,
                 boardsizeMax: 13,
                 isRemote: true,
                 requireMorePower: maxGames > 20,
-                pseudo: `Mohex ${level}`,
-                slug: `mohex-${level}`,
+                pseudo: pseudo ?? `Mohex ${level}`,
+                slug: slug ?? `mohex-${level}`,
             });
 
             if (!created) {
                 console.log(`Bot "${label}" already created`);
             }
         });
-
-        console.log(`Creating bot "Mohex"...`);
-
-        created = await createAiConfigIfNotExists({
-            config: { maxGames: 500000 },
-            engine: 'mohex',
-            label: 'Full',
-            description: 'not limited',
-            order: 25,
-            boardsizeMax: 13,
-            isRemote: true,
-            requireMorePower: true,
-            pseudo: 'Mohex',
-            slug: 'mohex',
-        });
-
-        if (!created) {
-            console.log('Bot "Mohex" already created');
-        }
 
         console.log('Mohex bots created.');
     })
