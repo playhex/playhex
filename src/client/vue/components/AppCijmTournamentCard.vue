adc8a1ffc936b0ea7683aabd8514d961f89071ce
Julien Maulny <julien.maulny@protonmail.com>
2025-06-10 20:59:36 +0200

    adjust cijm tournament card dates and display 'now'

--- src/client/vue/components/AppCijmTournamentCard.vue
+++ src/client/vue/components/AppCijmTournamentCard.vue
@@ -1,21 +1,39 @@
 <script setup lang="ts">
 import { BIconChevronRight, BIconTrophy } from 'bootstrap-icons-vue';
-import { formatDistanceToNowStrict } from 'date-fns';
+import { formatDistanceStrict } from 'date-fns';
 
+const saturday = new Date('2025-06-14T14:00:00+0200');
+const saturdayEnd = new Date('2025-06-14T18:00:00+0200');
+const sunday = new Date('2025-06-15T14:00:00+0200');
+const sundayEnd = new Date('2025-06-15T18:00:00+0200');
+
+const now = new Date();
 </script>
 
 <template>
-    <div class="card card-bg-icon border-salon-math mb-4">
+    <div v-if="now < sundayEnd" class="card card-bg-icon border-salon-math mb-4">
         <BIconTrophy style="top: 0.5rem; right: -1rem; font-size: 5rem" class="text-salon-math" />
         <div class="card-body">
             <p class="m-0 lead">{{ $t('hex_tournament_paris.title') }}</p>
 
             <p class="m-0">
                 <small>
-                    {{ formatDistanceToNowStrict(new Date('2025-06-15'), {
-                        addSuffix: true,
-                        unit: 'day',
-                    }) }}
+                    <template v-if="now < saturday">
+                        {{ formatDistanceStrict(saturday, now, {
+                            addSuffix: true,
+                        }) }}
+                    </template>
+                    <span v-else-if="now < saturdayEnd" class="text-warning">
+                        {{ $t('now!') }}
+                    </span>
+                    <template v-else-if="now < sunday">
+                        {{ formatDistanceStrict(sunday, now, {
+                            addSuffix: true,
+                        }) }}
+                    </template>
+                    <span v-else class="text-warning">
+                        {{ $t('now!') }}
+                    </span>
                     <BIconChevronRight class="ms-2" style="font-size: 0.6em" />
                     <router-link :to="{ name: 'cijm' }" class="stretched-link">
                         {{ $t('hex_tournament_paris.read_more') }}
