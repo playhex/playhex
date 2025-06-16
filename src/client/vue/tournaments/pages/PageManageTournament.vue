8e761a712f8d7754295c0cb777b7ca845e721438
Julien Maulny <julien.maulny@protonmail.com>
2025-06-16 23:23:45 +0200

    tournaments

---
+++ src/client/vue/tournaments/pages/PageManageTournament.vue
@@ -0,0 +1,235 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import i18next from 'i18next';
+import { apiDeleteTournamentBannedPlayer, apiDeleteTournamentSubscription, apiGetTournamentBannedPlayers, apiPatchTournament, apiPostIterateTournament, apiPostStartTournament, apiPutTournamentBannedPlayer } from '../../../apiClient.js';
+import { useTournamentFromUrl } from '../composables/tournamentFromUrl.js';
+import TournamentBannedPlayer from '../../../../shared/app/models/TournamentBannedPlayer.js';
+import Player from '../../../../shared/app/models/Player.js';
+import { DomainHttpError } from '../../../../shared/app/DomainHttpError.js';
+import useToastsStore from '../../../stores/toastsStore.js';
+import { Toast } from '../../../../shared/app/Toast.js';
+import { useHead } from '@unhead/vue';
+import TournamentSubscription from '../../../../shared/app/models/TournamentSubscription.js';
+
+const {
+    tournament,
+    slug,
+    iAmHost,
+} = useTournamentFromUrl();
+
+useHead({
+    title: () => (false === tournament.value ? null : tournament.value?.title) ?? 'Manage tournament',
+});
+
+/**
+ * Convert startAt Date to "datetime-local" input format (e.g "2025-04-01T22:00")
+ */
+const startsAtDatetimeLocal = computed<string>({
+    get(): string
+    {
+        if (!tournament.value) {
+            return '';
+        }
+
+        return tournament.value.startOfficialAt.toLocaleString('sv').replace(' ', 'T').substring(0, 16);
+    },
+
+    set(newValue: string): void
+    {
+        if (!tournament.value) {
+            return;
+        }
+
+        tournament.value.startOfficialAt = new Date(newValue + ':00');
+    },
+});
+
+const editTournament = async () => {
+    if (!tournament.value) {
+        return;
+    }
+
+    await apiPatchTournament(tournament.value);
+};
+
+const iterateTournament = async () => {
+    await apiPostIterateTournament(slug);
+};
+
+const startTournament = async () => {
+    try {
+        await apiPostStartTournament(slug);
+    } catch (e) {
+        if (e instanceof DomainHttpError) {
+            if ('tournament_not_enough_participants_to_start' === e.type) {
+                useToastsStore().addToast(new Toast(
+                    i18next.t(e.type),
+                    {
+                        level: 'danger',
+                        autoCloseAfter: 6000,
+                    },
+                ));
+
+                return;
+            }
+        }
+
+        throw e;
+    }
+};
+
+const kickPlayer = async (subscription: TournamentSubscription): Promise<void> => {
+    await apiDeleteTournamentSubscription(slug, subscription.player.publicId);
+
+    if (tournament.value) {
+        tournament.value.subscriptions = tournament.value.subscriptions
+            .filter(s => s.player.publicId !== subscription.player.publicId)
+        ;
+    }
+};
+
+/*
+ * Banned players
+ */
+const tournamentBannedPlayers = ref<null | TournamentBannedPlayer[]>(null);
+
+(async () => {
+    tournamentBannedPlayers.value = await apiGetTournamentBannedPlayers(slug);
+})();
+
+const banPlayer = async (player: Player): Promise<void> => {
+    const tournamentBannedPlayer = await apiPutTournamentBannedPlayer(slug, player);
+
+    if (null === tournamentBannedPlayers.value) {
+        return;
+    }
+
+    const alreadyListed = tournamentBannedPlayers.value
+        .some(bannedPlayer => bannedPlayer.player.publicId === player.publicId)
+    ;
+
+    if (!alreadyListed) {
+        tournamentBannedPlayers.value.push(tournamentBannedPlayer);
+    }
+
+    if (tournament.value) {
+        tournament.value.subscriptions = tournament.value.subscriptions
+            .filter(subscription => subscription.player.publicId !== player.publicId)
+        ;
+    }
+};
+
+const unbanPlayer = async (player: Player): Promise<void> => {
+    await apiDeleteTournamentBannedPlayer(slug, player);
+
+    if (null !== tournamentBannedPlayers.value) {
+        tournamentBannedPlayers.value = tournamentBannedPlayers.value
+            .filter(bannedPlayer => bannedPlayer.player.publicId !== player.publicId)
+        ;
+    }
+
+};
+</script>
+
+<template>
+    <div class="container my-3">
+        <router-link
+            v-if="iAmHost()"
+            :to="{ name: 'tournament', params: { slug }}"
+            class="btn btn-outline-primary float-end"
+        >
+            Back
+        </router-link>
+
+        <h1>Manage tournament <span v-if="tournament">{{ tournament.title }}</span></h1>
+
+        <template v-if="tournament && 'created' === tournament.state">
+            <h2>Edit</h2>
+
+            <p v-if="false === iAmHost()" class="text-warning">Tournament can be edited by host only.</p>
+
+            <form v-if="tournament" @submit.prevent="editTournament">
+                <div class="mb-3">
+                    <label for="name" class="form-label">Tournament name</label>
+                    <input
+                        v-model="tournament.title"
+                        type="text"
+                        class="form-control form-control-lg"
+                        id="name"
+                    >
+                </div>
+
+                <div class="mb-3">
+                    <label for="startDate" class="form-label">Start date</label>
+                    <input
+                        type="datetime-local"
+                        class="form-control form-control-lg"
+                        id="startDate"
+                        v-model="startsAtDatetimeLocal"
+                    >
+                </div>
+
+                <h3>Description, rules</h3>
+
+                <textarea
+                    v-model="tournament.description"
+                    class="form-control mb-3"
+                    id="tournament-description"
+                ></textarea>
+
+                <button type="submit" class="btn btn-success">Submit modifications</button>
+            </form>
+        </template>
+
+        <template v-if="tournament && 'created' === tournament.state">
+            <h2>Subscribers</h2>
+
+            <ul>
+                <li
+                    v-for="subscription in tournament.subscriptions"
+                    :key="subscription.player.publicId"
+                >
+                    <button @click="kickPlayer(subscription)" class="btn btn-sm btn-outline-warning me-2">Kick</button>
+                    <button @click="banPlayer(subscription.player)" class="btn btn-sm btn-outline-danger me-2">Kick & Ban</button>
+                    {{ subscription.player.pseudo }}
+                    <span v-if="subscription.checkedIn">(checked-in)</span>
+                </li>
+            </ul>
+        </template>
+
+        <h2>Banned players</h2>
+
+        <p v-if="null === tournamentBannedPlayers">{{ $t('loading') }}</p>
+        <p v-else-if="0 === tournamentBannedPlayers.length">(none)</p>
+        <ul v-else>
+            <li
+                v-for="tournamentBannedPlayer in tournamentBannedPlayers"
+                :key="tournamentBannedPlayer.player.publicId"
+            >
+                <button @click="unbanPlayer(tournamentBannedPlayer.player)" class="btn btn-sm btn-outline-success me-2">Unban</button>
+                {{ tournamentBannedPlayer.player.pseudo }}
+            </li>
+        </ul>
+
+        <h2>Manual actions</h2>
+
+        <template v-if="tournament && 'created' === tournament.state">
+            <button
+                @click="startTournament"
+                class="btn btn-success"
+            >Start tournament now</button>
+        </template>
+
+        <br>
+
+        <template v-if="tournament && 'ended' !== tournament.state">
+            <button @click="iterateTournament" class="btn btn-warning">Progress now</button>
+            <p><small>In case tournament seems stuck (next games not starting), this button should fix it by checking whole tournament state</small></p>
+        </template>
+    </div>
+</template>
+
+<style lang="stylus" scoped>
+h2, h3, h4
+    margin-top 1.5em
+</style>
---
+++ src/shared/app/tournamentUtils.ts
@@ -0,0 +1,165 @@
+import { HostedGameOptions, Player, Tournament, TournamentGame, TournamentParticipant } from './models/index.js';
+import { slugify } from './slugify.js';
+
+export const tournamentFormatStage1Values = [
+    'single-elimination',
+    'double-elimination',
+    'stepladder',
+    'swiss',
+    'round-robin',
+    'double-round-robin',
+] as const;
+
+export const tournamentFormatStage2Values = [
+    'single-elimination',
+    'double-elimination',
+    'stepladder',
+] as const;
+
+export type TournamentFormatStage1 = (typeof tournamentFormatStage1Values)[number];
+export type TournamentFormatStage2 = (typeof tournamentFormatStage2Values)[number];
+
+export const slugifyTournamentName = (tournamentName: string): string => slugify(tournamentName);
+
+export const createGameOptionsForTournament = (tournament: Tournament): HostedGameOptions => {
+    const gameOptions = new HostedGameOptions();
+
+    gameOptions.boardsize = tournament.boardsize;
+    gameOptions.timeControl = tournament.timeControl;
+    gameOptions.ranked = tournament.ranked;
+
+    return gameOptions;
+};
+
+export const getCheckInOpensDate = (tournament: Tournament): Date => {
+    return new Date(tournament.startOfficialAt.getTime() - tournament.checkInOpenOffsetSeconds * 1000);
+};
+
+export const isCheckInOpen = (tournament: Tournament, date: Date = new Date()): boolean => {
+    return date >= getCheckInOpensDate(tournament);
+};
+
+/**
+ * @returns Date when tournament should starts automatically, counting delayStart.
+ *      Or null if auto start is disabled by host.
+ */
+export const tournamentStartsAutomatically = (tournament: Tournament): null | Date => {
+    const { startOfficialAt: officiallyStartsAt, startDelayInSeconds: delayStartInSeconds } = tournament;
+
+    if (delayStartInSeconds < 0) {
+        return null;
+    }
+
+    return new Date(officiallyStartsAt.getTime() + delayStartInSeconds * 1000);
+};
+
+/**
+ * @returns "1.3" for tournament game round 1, number 3
+ */
+export const tournamentMatchNumber = (tournamentGame: TournamentGame): string => {
+    return tournamentGame.round + '.' + tournamentGame.number;
+};
+
+/**
+ * Sort participants from their score/tiebreak,
+ * and set them a rank.
+ */
+export const sortAndRankParticipants = (participants: TournamentParticipant[]): void => {
+    if (0 === participants.length) {
+        return;
+    }
+
+    participants.sort((a, b) => {
+        if (a.score === b.score) {
+            return b.tiebreak - a.tiebreak;
+        }
+
+        return b.score - a.score;
+    });
+
+    participants[0].rank = 1;
+
+    for (let i = 1; i < participants.length; ++i) {
+        const participant = participants[i];
+        const previous = participants[i - 1];
+
+        if (participant.score === previous.score && participant.tiebreak === previous.tiebreak) {
+            participant.rank = previous.rank;
+        } else {
+            participant.rank = i + 1;
+        }
+    }
+};
+
+/**
+ * Sort tournament participants by their rank.
+ * If rank is undefined, place them at the end.
+ */
+export const byRank = (a: TournamentParticipant, b: TournamentParticipant): number => {
+    return (a.rank ?? Infinity) - (b.rank ?? Infinity);
+};
+
+/**
+ * Creates an array of rounds, each containing games sorted by match number.
+ * E.g: [[1.1, 1.2, 1.3, 1.4], [2.1, 2.2], [3.1]]
+ */
+export const getRoundsFromTournament = (tournament: Tournament): TournamentGame[][] => {
+    const { participants } = tournament;
+    const players: { [publicId: string]: Player } = {};
+
+    for (const participant of participants) {
+        players[participant.player.publicId] = participant.player;
+    }
+
+    const rounds: TournamentGame[][] = [];
+    const totalRounds = getLastRound(tournament);
+
+    for (let i = 0; i < totalRounds; ++i) {
+        rounds.push([]);
+    }
+
+    for (const tournamentGame of tournament.games) {
+        rounds[tournamentGame.round - 1].push(tournamentGame);
+    }
+
+    for (const games of rounds) {
+        games.sort((a, b) => a.number - b.number);
+    }
+
+    return rounds;
+};
+
+export const getRoundAndNumberFromString = (roundAndNumber: string): { round: number, number: number } => {
+    const [round, number] = roundAndNumber.split('.');
+
+    return {
+        round: parseInt(round, 10),
+        number: parseInt(number, 10),
+    };
+};
+
+export const findTournamentGameByRoundAndNumber = (tournament: Tournament, round: number, number: number): null | TournamentGame => {
+    return tournament.games
+        .find(game => game.round === round && game.number === number)
+        ?? null
+    ;
+};
+
+export const getLastRound = (tournament: Tournament): number => {
+    return tournament.games.reduce(
+        (max, game) => Math.max(max, game.round),
+        0,
+    );
+};
+
+export const getDoubleEliminationFinalRound = (tournament: Tournament): number => {
+    const pathEnd = tournament.games
+        .find(({ winnerPath, loserPath }) => null === winnerPath && null === loserPath)
+    ;
+
+    if (!pathEnd) {
+        return Infinity;
+    }
+
+    return pathEnd.round;
+};
