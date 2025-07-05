e35997c3cbb3d6d5822c2c24dea2b99e4e4f6e3c
Julien Maulny <julien.maulny@protonmail.com>
2025-07-05 23:08:04 +0200

    Add missing space in time control label

--- src/client/vue/components/AppTimeControlLabel.vue
+++ src/client/vue/components/AppTimeControlLabel.vue
@@ -49,6 +49,7 @@ const cadency = timeControlToCadencyName(props.timeControlBoardsize);
         <span>{{ timeControlToString(timeControlBoardsize.timeControl) }}</span>
 
         <template v-if="showAverage">
+            <span>&nbsp;</span>
             <small class="text-body-secondary d-none d-sm-inline">(~{{ msToDuration(1000 * Math.round(calcAverageSecondsPerMove(timeControlBoardsize.timeControl, timeControlBoardsize.boardsize)), 1) }}&nbsp;/&nbsp;{{ $t('move') }})</small>
         </template>
     </span>
