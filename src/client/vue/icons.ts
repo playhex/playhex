2321c6e63e45b435e32abde1a6ab0b9510ecd64b
Julien Maulny <julien.maulny@protonmail.com>
2025-08-17 21:36:42 +0200

    use one-line exports for icons

--- src/client/vue/icons.ts
+++ src/client/vue/icons.ts
@@ -4,162 +4,81 @@
  * Using unplugin-icons: this solution supports tree-shaking.
  */
 
-import Icon0CircleFill from '~icons/bi/0-circle-fill';
-import IconAlarmFill from '~icons/bi/alarm-fill';
-import IconAlphabet from '~icons/bi/alphabet';
-import IconArrowBarLeft from '~icons/bi/arrow-bar-left';
-import IconArrowBarRight from '~icons/bi/arrow-bar-right';
-import IconArrowCounterclockwise from '~icons/bi/arrow-counterclockwise';
-import IconArrowDownUp from '~icons/bi/arrow-down-up';
-import IconArrowRight from '~icons/bi/arrow-right';
-import IconAspectRatio from '~icons/bi/aspect-ratio';
-import IconBell from '~icons/mdi/bell-outline';
-import IconBellCheckFill from '~icons/mdi/bell-check';
-import IconBookmarks from '~icons/bi/bookmarks';
-import IconBoxArrowRight from '~icons/bi/box-arrow-right';
-import IconBrightnessHighFill from '~icons/bi/brightness-high-fill';
-import IconCalendar from '~icons/bi/calendar';
-import IconCalendarEvent from '~icons/bi/calendar-event';
-import IconCaretDownFill from '~icons/bi/caret-down-fill';
-import IconCaretRight from '~icons/bi/caret-right';
-import IconCaretUpFill from '~icons/bi/caret-up-fill';
-import IconChatDots from '~icons/bi/chat-dots';
-import IconCheck from '~icons/bi/check';
-import IconChevronBarLeft from '~icons/bi/chevron-bar-left';
-import IconChevronBarRight from '~icons/bi/chevron-bar-right';
-import IconChevronLeft from '~icons/bi/chevron-left';
-import IconChevronRight from '~icons/bi/chevron-right';
-import IconCircle from '~icons/fa/circle-o';
-import IconCircleFill from '~icons/bi/circle-fill';
-import IconCircleHalf from '~icons/bi/circle-half';
-import IconCrosshair from '~icons/bi/crosshair';
-import IconDiscord from '~icons/bi/discord';
-import IconDot from '~icons/bi/dot';
-import IconDownload from '~icons/bi/download';
-import IconExclamationTriangle from '~icons/bi/exclamation-triangle';
-import IconExclamationTriangleFill from '~icons/bi/exclamation-triangle-fill';
-import IconEye from '~icons/bi/eye';
-import IconFileEarmarkText from '~icons/bi/file-earmark-text';
-import IconFlag from '~icons/bi/flag';
-import IconGear from '~icons/bi/gear';
-import IconGithub from '~icons/bi/github';
-import IconHexagon from '~icons/bi/hexagon';
-import IconHexagonFill from '~icons/bi/hexagon-fill';
-import IconHourglass from '~icons/bi/hourglass';
-import IconHouse from '~icons/bi/house';
-import IconInfoCircle from '~icons/bi/info-circle';
-import IconInfoCircleFill from '~icons/bi/info-circle-fill';
-import IconInfoLg from '~icons/bi/info-lg';
-import IconLightningChargeFill from '~icons/bi/lightning-charge-fill';
-import IconList from '~icons/bi/list';
-import IconMoonFill from '~icons/bi/moon-fill';
-import IconMoonStarsFill from '~icons/bi/moon-stars-fill';
-import IconPcDisplayHorizontal from '~icons/bi/pc-display-horizontal';
-import IconPeople from '~icons/bi/people';
-import IconPeopleFill from '~icons/bi/people-fill';
-import IconPerson from '~icons/bi/person';
-import IconPersonFill from '~icons/bi/person-fill';
-import IconPersonUp from '~icons/bi/person-up';
-import IconPhone from '~icons/bi/phone';
-import IconPlayFill from '~icons/bi/play-fill';
-import IconPlus from '~icons/bi/plus';
-import IconRecordFill from '~icons/bi/record-fill';
-import IconRepeat from '~icons/bi/repeat';
-import IconRewind from '~icons/bi/rewind';
-import IconRobot from '~icons/bi/robot';
-import IconScissors from '~icons/bi/scissors';
-import IconSearch from '~icons/bi/search';
-import IconSendFill from '~icons/bi/send-fill';
-import IconShareFill from '~icons/bi/share-fill';
-import IconSignpostSplit from '~icons/bi/signpost-split';
-import IconSignTurnLeft from '~icons/bi/sign-turn-left';
-import IconThreeDots from '~icons/bi/three-dots';
-import IconTrash from '~icons/bi/trash';
-import IconTrophy from '~icons/bi/trophy';
-import IconTrophyFill from '~icons/bi/trophy-fill';
-import IconX from '~icons/bi/x';
-import IconWifiOff from '~icons/bi/wifi-off';
-import IconXLg from '~icons/bi/x-lg';
-import IconYoutube from '~icons/bi/youtube';
-import IconZoomOut from '~icons/bi/zoom-out';
-
-export {
-    Icon0CircleFill,
-    IconAlarmFill,
-    IconAlphabet,
-    IconArrowBarLeft,
-    IconArrowBarRight,
-    IconArrowCounterclockwise,
-    IconArrowDownUp,
-    IconArrowRight,
-    IconAspectRatio,
-    IconBell,
-    IconBellCheckFill,
-    IconBookmarks,
-    IconBoxArrowRight,
-    IconBrightnessHighFill,
-    IconCalendar,
-    IconCalendarEvent,
-    IconCaretDownFill,
-    IconCaretRight,
-    IconCaretUpFill,
-    IconChatDots,
-    IconCheck,
-    IconChevronBarLeft,
-    IconChevronBarRight,
-    IconChevronLeft,
-    IconChevronRight,
-    IconCircle,
-    IconCircleFill,
-    IconCircleHalf,
-    IconCrosshair,
-    IconDiscord,
-    IconDot,
-    IconDownload,
-    IconExclamationTriangle,
-    IconExclamationTriangleFill,
-    IconEye,
-    IconFileEarmarkText,
-    IconFlag,
-    IconGear,
-    IconGithub,
-    IconHexagon,
-    IconHexagonFill,
-    IconHourglass,
-    IconHouse,
-    IconInfoCircle,
-    IconInfoCircleFill,
-    IconInfoLg,
-    IconLightningChargeFill,
-    IconList,
-    IconMoonFill,
-    IconMoonStarsFill,
-    IconPcDisplayHorizontal,
-    IconPeople,
-    IconPeopleFill,
-    IconPerson,
-    IconPersonFill,
-    IconPersonUp,
-    IconPhone,
-    IconPlayFill,
-    IconPlus,
-    IconRecordFill,
-    IconRepeat,
-    IconRewind,
-    IconRobot,
-    IconScissors,
-    IconSearch,
-    IconSendFill,
-    IconShareFill,
-    IconSignpostSplit,
-    IconSignTurnLeft,
-    IconThreeDots,
-    IconTrash,
-    IconTrophy,
-    IconTrophyFill,
-    IconWifiOff,
-    IconX,
-    IconXLg,
-    IconYoutube,
-    IconZoomOut,
-};
+export { default as Icon0CircleFill } from '~icons/bi/0-circle-fill';
+export { default as IconAlarmFill } from '~icons/bi/alarm-fill';
+export { default as IconAlphabet } from '~icons/bi/alphabet';
+export { default as IconArrowBarLeft } from '~icons/bi/arrow-bar-left';
+export { default as IconArrowBarRight } from '~icons/bi/arrow-bar-right';
+export { default as IconArrowCounterclockwise } from '~icons/bi/arrow-counterclockwise';
+export { default as IconArrowDownUp } from '~icons/bi/arrow-down-up';
+export { default as IconArrowRight } from '~icons/bi/arrow-right';
+export { default as IconAspectRatio } from '~icons/bi/aspect-ratio';
+export { default as IconBell } from '~icons/mdi/bell-outline';
+export { default as IconBellCheckFill } from '~icons/mdi/bell-check';
+export { default as IconBookmarks } from '~icons/bi/bookmarks';
+export { default as IconBoxArrowRight } from '~icons/bi/box-arrow-right';
+export { default as IconBrightnessHighFill } from '~icons/bi/brightness-high-fill';
+export { default as IconCalendar } from '~icons/bi/calendar';
+export { default as IconCalendarEvent } from '~icons/bi/calendar-event';
+export { default as IconCaretDownFill } from '~icons/bi/caret-down-fill';
+export { default as IconCaretRight } from '~icons/bi/caret-right';
+export { default as IconCaretUpFill } from '~icons/bi/caret-up-fill';
+export { default as IconChatDots } from '~icons/bi/chat-dots';
+export { default as IconCheck } from '~icons/bi/check';
+export { default as IconChevronBarLeft } from '~icons/bi/chevron-bar-left';
+export { default as IconChevronBarRight } from '~icons/bi/chevron-bar-right';
+export { default as IconChevronLeft } from '~icons/bi/chevron-left';
+export { default as IconChevronRight } from '~icons/bi/chevron-right';
+export { default as IconCircle } from '~icons/fa/circle-o';
+export { default as IconCircleFill } from '~icons/bi/circle-fill';
+export { default as IconCircleHalf } from '~icons/bi/circle-half';
+export { default as IconCrosshair } from '~icons/bi/crosshair';
+export { default as IconDiscord } from '~icons/bi/discord';
+export { default as IconDot } from '~icons/bi/dot';
+export { default as IconDownload } from '~icons/bi/download';
+export { default as IconExclamationTriangle } from '~icons/bi/exclamation-triangle';
+export { default as IconExclamationTriangleFill } from '~icons/bi/exclamation-triangle-fill';
+export { default as IconEye } from '~icons/bi/eye';
+export { default as IconFileEarmarkText } from '~icons/bi/file-earmark-text';
+export { default as IconFlag } from '~icons/bi/flag';
+export { default as IconGear } from '~icons/bi/gear';
+export { default as IconGithub } from '~icons/bi/github';
+export { default as IconHexagon } from '~icons/bi/hexagon';
+export { default as IconHexagonFill } from '~icons/bi/hexagon-fill';
+export { default as IconHourglass } from '~icons/bi/hourglass';
+export { default as IconHouse } from '~icons/bi/house';
+export { default as IconInfoCircle } from '~icons/bi/info-circle';
+export { default as IconInfoCircleFill } from '~icons/bi/info-circle-fill';
+export { default as IconInfoLg } from '~icons/bi/info-lg';
+export { default as IconLightningChargeFill } from '~icons/bi/lightning-charge-fill';
+export { default as IconList } from '~icons/bi/list';
+export { default as IconMoonFill } from '~icons/bi/moon-fill';
+export { default as IconMoonStarsFill } from '~icons/bi/moon-stars-fill';
+export { default as IconPcDisplayHorizontal } from '~icons/bi/pc-display-horizontal';
+export { default as IconPeople } from '~icons/bi/people';
+export { default as IconPeopleFill } from '~icons/bi/people-fill';
+export { default as IconPerson } from '~icons/bi/person';
+export { default as IconPersonFill } from '~icons/bi/person-fill';
+export { default as IconPersonUp } from '~icons/bi/person-up';
+export { default as IconPhone } from '~icons/bi/phone';
+export { default as IconPlayFill } from '~icons/bi/play-fill';
+export { default as IconPlus } from '~icons/bi/plus';
+export { default as IconRecordFill } from '~icons/bi/record-fill';
+export { default as IconRepeat } from '~icons/bi/repeat';
+export { default as IconRewind } from '~icons/bi/rewind';
+export { default as IconRobot } from '~icons/bi/robot';
+export { default as IconScissors } from '~icons/bi/scissors';
+export { default as IconSearch } from '~icons/bi/search';
+export { default as IconSendFill } from '~icons/bi/send-fill';
+export { default as IconShareFill } from '~icons/bi/share-fill';
+export { default as IconSignpostSplit } from '~icons/bi/signpost-split';
+export { default as IconSignTurnLeft } from '~icons/bi/sign-turn-left';
+export { default as IconThreeDots } from '~icons/bi/three-dots';
+export { default as IconTrash } from '~icons/bi/trash';
+export { default as IconTrophy } from '~icons/bi/trophy';
+export { default as IconTrophyFill } from '~icons/bi/trophy-fill';
+export { default as IconX } from '~icons/bi/x';
+export { default as IconWifiOff } from '~icons/bi/wifi-off';
+export { default as IconXLg } from '~icons/bi/x-lg';
+export { default as IconYoutube } from '~icons/bi/youtube';
+export { default as IconZoomOut } from '~icons/bi/zoom-out';
